802e21d91e25f1f4a3c2e55bff06cb141f78c4e6
atomforevertoxic <139710109+atomforevertoxic@users.noreply.github.com>
2024-06-18 16:33:00 +0300

    Реализация функции getTextFromFile + исправление ошибок
    Функция getTextFromFile реализована на основе псевдокода, написанного в файле внешней спецификации программы, а также дополнена функционалом для отслеживания ошибок, возникающих во время считывания текста из стороннего файла. Изменен заголовок функции и соответствующий ему doxygen комментарий. Изменен код тестов функции getTextFromFile с целью тестирования отслеживаемых ошибок. Изменены все комментарии к заголовкам, чтобы исправить ошибку некорректного отображения doxygen подсказок

--- ConstantFinder.cpp
+++ ConstantFinder.cpp
@@ -5,9 +5,38 @@ int main()
     std::cout << "Hello World!\n";
 }
 
-list<string> getTextFromFile(const string pathAndName)
+list<string> getTextFromFile(const string pathAndName, ErrorInfo& error)
 {
+    //Открытие файлового потока
+    ifstream file(pathAndName);
+
     list<string> textCode;
+
+    //Если файл не получилоось открыть 
+    if (!file.is_open())
+    {
+        //Выдать соответствующее сообщение об ошибке
+        error = ErrorInfo(-1, ErrorMessage::INVALID_FILENAME_OR_FILEPATH_OR_FILE_DOES_NOT_EXIST);
+    }
+
+    string line;
+    bool wasStrErased = false;
+    //Пока удается получить строку
+    while (getline(file, line))
+    {
+        //Если строка еще не была обрезана
+        if (!wasStrErased)
+        {
+            //Вырезаются первые 3 "мусорных" символа из первой строки текста
+            line.erase(0, 3);
+            //Устанавливается флаг совершенного удаления
+            wasStrErased = true;
+        }
+        //Добавление строки в конец списка
+        textCode.push_back(line);
+    }
+    //Закрытие файлового потока
+    file.close();
 
     return textCode;
 }
