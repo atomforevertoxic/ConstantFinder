a67a52009275b16ad9af9dd7c6a9461dd16f64e6
atomforevertoxic <139710109+atomforevertoxic@users.noreply.github.com>
2024-06-19 14:37:46 +0300

    Реализация функций, работающих с комментариями в тексте кода
    Реализованы тестируемые функции removeAllCommentsFromString, getLeftmostComment и isConstFindsEarlierThanComments, а также вспомогательные нетестируемые функции согласно псевдокоду и логике программы

--- ConstantFinder.cpp
+++ ConstantFinder.cpp
@@ -62,15 +62,78 @@ vector<ErrorInfo> isInputDataValid(const list<string>& codeText)
 
 bool removeAllCommentsFromString(string& strToExecute)
 {
+    SubstrPos commaPos = getLeftmostComment(strToExecute);
+    while (commaPos.left != -1)
+    {
+        if (commaPos.right == -1)
+        {
+            commaPos.right = strToExecute.length() - 1;
+            cleanSubstrBySpaces(strToExecute, commaPos);
+            return true;
+        }
+        else
+        {
+            cleanSubstrBySpaces(strToExecute, commaPos);
+            commaPos = getLeftmostComment(strToExecute);
+        }
+    }
     return false;
 }
 
 SubstrPos getLeftmostComment(string& strToResearch)
 {
-    return SubstrPos();
+    SubstrPos leftmostCommaPos;
+    int startSearchingForLeftmostComma = 0;
+    while (isConstFindsEarlierThanComments(strToResearch, startSearchingForLeftmostComma))
+    {
+        int newStartPos = getSubstrPositionOrDefaultValue(strToResearch, "\"", startSearchingForLeftmostComma) + 2;
+        startSearchingForLeftmostComma = newStartPos;
+    }
+
+    int singleLineCommentIndex = getSubstrPositionOrDefaultValue(strToResearch, "//", startSearchingForLeftmostComma);
+    int multLineCommentIndex = getSubstrPositionOrDefaultValue(strToResearch, "/*", startSearchingForLeftmostComma);
+    if (multLineCommentIndex < singleLineCommentIndex)
+    {
+        leftmostCommaPos.left = multLineCommentIndex;
+        int multLineCommaEndIndex = getSubstrPositionOrDefaultValue(strToResearch, "*/", startSearchingForLeftmostComma);
+        if (multLineCommaEndIndex == strToResearch.length())
+        {
+            leftmostCommaPos.right = -1;
+        }
+        else
+        {
+            leftmostCommaPos.right = multLineCommaEndIndex + 1;
+        }
+    }
+    else if (singleLineCommentIndex < multLineCommentIndex)
+    {
+        leftmostCommaPos.left = singleLineCommentIndex;
+        leftmostCommaPos.right = strToResearch.length() - 1;
+    }
+    return leftmostCommaPos;
+}
+
+void cleanSubstrBySpaces(string& strToClean, SubstrPos pos)
+{
+    for (int i = pos.left; i <= pos.right; i++)
+    {
+        strToClean[i] = ' ';
+    }
 }
 
 bool isConstFindsEarlierThanComments(const string& strToSearch, int startSearching)
 {
-    return false;
+    int strConstIndex = getSubstrPositionOrDefaultValue(strToSearch, "\"", startSearching);
+    int singleLineCommentIndex = getSubstrPositionOrDefaultValue(strToSearch, "//", startSearching);
+    int multLinesCommentIndex = getSubstrPositionOrDefaultValue(strToSearch, "/*", startSearching);
+    list<int> indexes = { strConstIndex, singleLineCommentIndex, multLinesCommentIndex };
+    int minElem = *min_element(indexes.begin(), indexes.end());
+    return minElem == strConstIndex && minElem != strToSearch.length();
+}
+
+int getSubstrPositionOrDefaultValue(const string& strToCheck, const string& substrToFind, int startSearching)
+{
+    int posOfSubstr = strToCheck.find(substrToFind, startSearching);
+    posOfSubstr = posOfSubstr == -1 ? strToCheck.length() : posOfSubstr;
+    return posOfSubstr;
 }
